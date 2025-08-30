df557287b6d822be2a57b94e681b92061f83abfc
sathishrouthu <sathishrouthu222@gmail.com>
2025-08-30 20:56:21 +0530

    cors error

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
-from app.routes.mail_routes import mail_bp
+from flask_cors import CORS  # Import CORS
+
 
 def create_app():
     app = Flask(__name__)
 
-    # Configurations
-    app.config.from_object('app.config.Config')
+    # Enable CORS for all domains (useful for development)
+    CORS(app)
 
-    # Register Blueprints (Routes)
+    # Register blueprints and routes
+    from app.routes.mail_routes import mail_bp
     app.register_blueprint(mail_bp)
 
     return app
