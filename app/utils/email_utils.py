b7238fdfadb4b0328ad42f77e75f7b4613fe5a38
sathishrouthu <sathishrouthu222@gmail.com>
2025-08-30 22:06:04 +0530

    html mail

--- app/utils/email_utils.py
+++ app/utils/email_utils.py
@@ -6,14 +6,18 @@ from flask import current_app
 
 def send_email(to_email, subject, body):
     try:
+        # Create a MIME message
         msg = MIMEMultipart()
         msg["From"] = current_app.config["SMTP_FROM_MAIL"]
         msg["To"] = to_email
         msg["Subject"] = subject
-        msg.attach(MIMEText(body, "plain"))
 
+        # Attach the HTML body to the email
+        msg.attach(MIMEText(body, "html"))  # Send as HTML email
+
+        # Connect to the SMTP server and send the email
         with smtplib.SMTP(current_app.config["SMTP_SERVER"], current_app.config["SMTP_PORT"]) as server:
-            server.starttls()
+            server.starttls()  # Start TLS (secure connection)
             server.login(current_app.config["SMTP_USERNAME"], current_app.config["SMTP_PASSWORD"])
             server.send_message(msg)
 
